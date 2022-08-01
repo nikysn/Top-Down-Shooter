ec4533282c7e0db93181830d517d8c1a6bb6ea8b
nikysn <nikysn@yandex.ru>
2022-08-01 09:37:14 +0300

    codeFix

--- Assets/Scripts/Spawner/ObjectPool.cs
+++ Assets/Scripts/Spawner/ObjectPool.cs
@@ -11,25 +11,25 @@ public class ObjectPool : MonoBehaviour
     [SerializeField] private int _capacity;
     [SerializeField] Player _target;
     
-    private List<Enemy> _pools = new List<Enemy>();
+   
 
 
    // public event UnityAction<int, int> EnemyCountChanged;
 
-    protected void Initialize(Enemy prefab)
+    protected void Initialize(Enemy prefab,List<Enemy> pools)
     {
         for (int i = 0; i < _capacity; i++)
         {
             Enemy spawned = Instantiate(prefab, _container.transform);
             spawned.Init(_target);
             spawned.gameObject.SetActive(false);
-            _pools.Add(spawned);
+            pools.Add(spawned);
         }
     }
 
-    protected bool TryGetObject(out Enemy result)
+    protected bool TryGetObject(out Enemy result, List<Enemy> pools)
     {
-        result = _pools.FirstOrDefault(p => p.gameObject.activeSelf == false);
+        result = pools.FirstOrDefault(p => p.gameObject.activeSelf == false);
 
         return result != null;
     }
--- Assets/Scripts/Spawner/Spawner.cs
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -7,6 +7,7 @@ public class Spawner : ObjectPool
     [SerializeField] private Enemy _enemyPrefab;
     [SerializeField] private float _secondsBetweenSpawn;
     [SerializeField] private Transform[] _spawnPoints;
+    private List<Enemy> _poolsBird = new List<Enemy>();
 
     private float _elapserTime = 0;
 
@@ -19,25 +20,48 @@ public class Spawner : ObjectPool
 
     private void Start()
     {
-        Initialize(_enemyPrefab);
+        Initialize(_enemyPrefab, _poolsBird);
+    }
+
+    private IEnumerator SpawningEnemy()
+    {
+
+        if (TryGetObject(out Enemy enemy, _poolsBird))
+        {
+            
+            int spawnPointNumber = Random.Range(0, _spawnPoints.Length);
+
+            SetEnemy(enemy, _spawnPoints[spawnPointNumber].position);
+            yield return new WaitForSeconds(5f);
+
+        }
+        // if (TryGetObject(out Enemy enemy, _poolsBird))
+        // {
+        // _elapserTime = 0;
+
+
+
+        // }
     }
 
     private void Update()
     {
-        _elapserTime += Time.deltaTime;
+        /* _elapserTime += Time.deltaTime;
 
-        if (_elapserTime >= _secondsBetweenSpawn)
-        {
-            if (TryGetObject(out Enemy enemy))
-            {
-                _elapserTime = 0;
+         if (_elapserTime >= _secondsBetweenSpawn)
+         {
+             if (TryGetObject(out Enemy enemy, _poolsBird))
+             {
+                 _elapserTime = 0;
 
-                int spawnPointNumber = Random.Range(0, _spawnPoints.Length);
-                
-                SetEnemy(enemy, _spawnPoints[spawnPointNumber].position);
- 
-            }
-        }
+                 int spawnPointNumber = Random.Range(0, _spawnPoints.Length);
+
+                 SetEnemy(enemy, _spawnPoints[spawnPointNumber].position);
+
+             }
+         }*/
+
+        
     }
 
 }
